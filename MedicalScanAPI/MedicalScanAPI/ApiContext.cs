f0d20045b35dee41ac046de8ca6f6a3c1533bb8e
Hauth Attila <hauth.attila@gmail.com>
2024-02-29 21:15:34 +0100

    Implement Context

---
+++ MedicalScanAPI/MedicalScanAPI/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using MedicalScanAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalScanAPI
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "MedicalScanDb");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
