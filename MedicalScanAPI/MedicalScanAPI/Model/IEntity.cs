3ef748918d06dd21ad614991ad302328285d9615
Hauth Attila <hauth.attila@gmail.com>
2024-03-01 12:31:38 +0100

    Add IEntity interface

---
+++ MedicalScanAPI/MedicalScanAPI/Model/IEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace MedicalScanAPI.Model
+{
+    public interface IEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
--- MedicalScanAPI/MedicalScanAPI/Model/Product.cs
+++ MedicalScanAPI/MedicalScanAPI/Model/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace MedicalScanAPI.Model
 {
-    public class Product
+    public class Product : IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
