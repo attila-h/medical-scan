7d07aef0c6cefd3687ddbe7e781406ff816ca27b
Hauth Attila <hauth.attila@gmail.com>
2024-03-04 16:21:48 +0100

    Add product module

--- medical-scan-frontend/src/app/app-routing.module.ts
+++ medical-scan-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductModule } from './product/product.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products'
+  },
+  {
+    path: 'products',
+    pathMatch: 'full',
+    loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+  }
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ProductModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
