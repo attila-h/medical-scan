9b7a261e6b5ba10869e9fc45ba1b169875b7659f
Hauth Attila <hauth.attila@gmail.com>
2024-03-04 19:56:05 +0100

    Fix baseUrl in env files

--- medical-scan-frontend/src/environments/environment.development.ts
+++ medical-scan-frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   isProduction: false,
   api: {
-    baseUrl: 'http://localhost:5167'
+    baseUrl: 'http://localhost:5167/api'
   }
 };
--- medical-scan-frontend/src/environments/environment.production.ts
+++ medical-scan-frontend/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   isProduction: true,
   api: {
-    baseUrl: 'http://localhost:5167'
+    baseUrl: 'http://localhost:5167/api'
   }
 };
