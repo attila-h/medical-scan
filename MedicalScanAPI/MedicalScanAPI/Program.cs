c2feb75b46183f0ac1f6afaddb9c5895dceb1d03
Hauth Attila <hauth.attila@gmail.com>
2024-03-06 23:49:42 +0100

    Setup cors

--- MedicalScanAPI/MedicalScanAPI/Program.cs
+++ MedicalScanAPI/MedicalScanAPI/Program.cs
@@ -15,6 +15,11 @@ builder.Services.AddDbContext<ApiContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => {
+    policy.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -24,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseAuthorization();
 
